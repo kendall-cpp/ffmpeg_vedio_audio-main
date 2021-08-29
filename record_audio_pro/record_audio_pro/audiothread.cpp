718f769367fb665d46b6466a90c91cef5bde2596
kendall-cpp <1456263654@qq.com>
2021-08-29 23:32:37 +0800

    修改保存文件名

--- record_audio_pro/record_audio_pro/audiothread.cpp
+++ record_audio_pro/record_audio_pro/audiothread.cpp
@@ -1,5 +1,7 @@
 ﻿#include "audiothread.h"
 #include <qdebug.h>
+#include <QString>
+#include <QTime>
 #include <QFile>
 
 extern "C" {
@@ -17,7 +19,7 @@ extern "C" {
     #define FMT_NAME  "dshow"
     #define DEVICE_NAME  "audio=麦克风阵列 (2- Realtek High Definition Audio)"
     // PCM 文件名
-    #define FILENAME "D:/lingshengYinShiPin/out.pcm"
+    #define FILEPATH "D:/lingshengYinShiPin/"
 #else
     #define FMT_NAME  "avfoundation"
     #define DEVICE_NAME ":0"
@@ -68,12 +70,14 @@ void AudioThread::run() {
     }
 
        //文件名
-    const char *filename = FILENAME;
+    QString filename = FILEPATH;
+    filename += QDateTime::currentDateTime().toString("MM_dd_HH_mm_ss");
+    filename += ".pcm";
     QFile file(filename);
 
      //以只写的方式打开文件，如果文件不存在就创建文件，存在就删除重新创建
     if(!file.open(QIODevice::WriteOnly )) {
-        qDebug() << "文件打开失败";
+        qDebug() << "文件打开失败" << filename;
         avformat_close_input(&ctx);
         return;
     }
@@ -82,19 +86,38 @@ void AudioThread::run() {
 //    int count = 50;
 
     //数据包
-    AVPacket pkt;
+//    AVPacket pkt;
     //不断采集数据 isInterruptionRequested 是否有人请求我停止
-    while( !isInterruptionRequested() && av_read_frame(ctx,&pkt) == 0) {
-        file.write((const char*)pkt.data,pkt.size);
-    }
+//    while( !isInterruptionRequested() && av_read_frame(ctx,&pkt) == 0) {
+//        file.write((const char*)pkt.data,pkt.size);
+//    }
 //    while( !_stop && av_read_frame(ctx,&pkt) == 0) {
 //        file.write((const char*)pkt.data,pkt.size);
 //    }
+    AVPacket *pkt = av_packet_alloc();
+    while(!isInterruptionRequested()) {
+        //不断采集
+        ret = av_read_frame(ctx,pkt);
+        if(ret == 0) {
+            //将数据写入文件
+            file.write(( const char *) pkt->data,pkt->size);
+        }else if (ret == AVERROR(EAGAIN)) { //资源临时不可用
+            continue;
+        } else { //其他错误
+            char errbuf[1024];
+            av_strerror(ret,errbuf,sizeof(errbuf));
+            qDebug() << "av_read_frame error" << errbuf << ret;
+            break;
+        }
+    }
+
+    //必须要加，释放pkt内部资源
+    av_packet_unref(pkt);
 
     //关闭文件
     file.close();
     //释放资源
-//    av_packet_free(&pkt);
+    av_packet_free(&pkt);
     //关闭设备
     avformat_close_input(&ctx);
 
